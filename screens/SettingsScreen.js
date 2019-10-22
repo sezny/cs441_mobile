0595ba2f61def65b64e50bf6f9aefb6c7fe99020
Damien <damsltc57@yahoo.fr>
2019-10-21 17:08:44 -0700

    fix(datepicker): Update date picker colors

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -14,71 +14,87 @@ let deviceWidth = Dimensions.get('window').width;
 export default function SettingsScreen() {
 
 
-   const[username, getUsername, setUsername] = useState('Cnixon12');
-   const[name] = useState('Curtis Nixon');
-   const[email] = useState('CN@gmail.com');
-   const[major] = useState('CS');
-   const[school] = useState('San Marcos'); 
-   const[startDate, setStartDate] = useState('10-21-2019');
-   const[endDate, setEndDate] = useState('');
-   const[eventDesc, setDesc] = useState('');
-   const[eventName, setEventName] = useState(''); 
-   
-   
-
-       function createEvent(){
-           
-       }
-
-   
-   /*getInfo(){
-       //Get info from Server(Name, Email, School, Major, and Schedule)
-       //go to https://facebook.github.io/react-native/docs/network for example
-   }*/
+    const[username, getUsername, setUsername] = useState('Cnixon12');
+    const[name] = useState('Curtis Nixon');
+    const[email] = useState('CN@gmail.com');
+    const[major] = useState('CS');
+    const[school] = useState('San Marcos');
+    const[startDate, setStartDate] = useState('10-21-2019');
+    const[endDate, setEndDate] = useState('');
+    const[eventDesc, setDesc] = useState('');
+    const[eventName, setEventName] = useState('');
 
 
-  return (
-  <ScrollView>
-    <View style = {styles.row}>
-            <Text style = {styles.item}>Username</Text>
-            <Text style = {styles.info}>{username}</Text>
-    </View>
-    <View style = {styles.row}>
-            <Text style = {styles.item}>Name</Text>
-            <Text style = {styles.info}>{name}</Text>
-    </View>
-    <View style = {styles.row}>
-            <Text style = {styles.item}>Email</Text>
-            <Text style = {styles.info}>{email}</Text>
-    </View>
-    <View style = {styles.row}>
-            <Text style = {styles.item}>Major</Text>
-            <Text style = {styles.info}>{major}</Text>
-    </View>
-    <View style = {styles.row}>
-            <Text style = {styles.item}>School</Text>
-            <Text style = {styles.info}>{school}</Text>
-    </View>
-    <View style={styles.scheduleDisplay}>
-        <WeekView selectedDate={new Date()} numberOfDays={3} headerStyle={styles.headerStyle} locale='fr'>
-        </WeekView>    
-   </View>
-   <View style = {styles.addEventView}>
-        <Text style={styles.addEventText}>Add Event to Schedule</Text>
-        <DatePicker style={styles.datePicker} date={startDate} mode='date' placeholder='select date' format="MM-DD-YYYY" minDate="05-01-2016" maxDate="05-01-2016" onDateChange={date=>setStartDate(date)} confirmBtnText ='Confirm' cancelBtnText='Cancel'/>
-        <DatePicker style={styles.datePicker} date={new Date()} mode='date' placeholder='select date' format="MM-DD-YYYY" minDate='09-30-2019' maxDate="12-31-2020" onDateChange={date=>setEndDate(date)} confirmBtnText ='Confirm' cancelBtnText='Cancel' />
-        <Input placeholder="Add event description here" value={eventDesc} onChangeText = {text => setDesc(text)}/>
-        <Button style={{marginTop:30}} size="large" onPress={() => createEvent()}>Add Event</Button>
-   </View>
-    <View>
-        <Button style={{marginTop: 30}} status="danger" size= "large" onPres={() => updatePersonalInfo()} >Update Personal Info</Button>
-    </View>
-  </ScrollView>
-  );
+
+    function createEvent(){
+
+    }
+
+
+    /*getInfo(){
+        //Get info from Server(Name, Email, School, Major, and Schedule)
+        //go to https://facebook.github.io/react-native/docs/network for example
+    }*/
+
+
+    return (
+        <ScrollView>
+            <View style = {styles.row}>
+                <Text style = {styles.item}>Username</Text>
+                <Text style = {styles.info}>{username}</Text>
+            </View>
+            <View style = {styles.row}>
+                <Text style = {styles.item}>Name</Text>
+                <Text style = {styles.info}>{name}</Text>
+            </View>
+            <View style = {styles.row}>
+                <Text style = {styles.item}>Email</Text>
+                <Text style = {styles.info}>{email}</Text>
+            </View>
+            <View style = {styles.row}>
+                <Text style = {styles.item}>Major</Text>
+                <Text style = {styles.info}>{major}</Text>
+            </View>
+            <View style = {styles.row}>
+                <Text style = {styles.item}>School</Text>
+                <Text style = {styles.info}>{school}</Text>
+            </View>
+            <View style={styles.scheduleDisplay}>
+                <WeekView selectedDate={new Date()} numberOfDays={3} headerStyle={styles.headerStyle} locale='fr'>
+                </WeekView>
+            </View>
+            <View style = {styles.addEventView}>
+                <Text style={styles.addEventText}>Add Event to Schedule</Text>
+                <DatePicker customStyles={{
+                    datePicker: { backgroundColor: '#222'  },
+                    datePickerCon: { backgroundColor: '#333' },
+                    dateIcon: {
+                        position: 'absolute',
+                        left: 0,
+                        top: 4,
+                        marginLeft: 0
+                    },
+                    dateInput: {
+                        marginLeft: 36
+                    },
+                    dateText: {
+                        color: 'black'
+                    },
+                    // ... You can check the source to find the other keys.
+                }} date={startDate} mode='date' placeholder='select date' format="MM-DD-YYYY" minDate="05-01-2016" maxDate="05-01-2020" onDateChange={date=>setStartDate(date)} confirmBtnText ='Confirm' cancelBtnText='Cancel'/>
+                <DatePicker style={styles.datePicker} date={new Date()} mode='date' placeholder='select date' format="MM-DD-YYYY" minDate='09-30-2019' maxDate="12-31-2020" onDateChange={date=>setEndDate(date)} confirmBtnText ='Confirm' cancelBtnText='Cancel' />
+                <Input placeholder="Add event description here" value={eventDesc} onChangeText = {text => setDesc(text)}/>
+                <Button style={{marginTop:30}} size="large" onPress={() => createEvent()}>Add Event</Button>
+            </View>
+            <View>
+                <Button style={{marginTop: 30}} status="danger" size= "large" onPres={() => updatePersonalInfo()} >Update Personal Info</Button>
+            </View>
+        </ScrollView>
+    );
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'Profile',
+    title: 'Profile',
 
 };
 const styles = StyleSheet.create({
@@ -100,7 +116,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#4286f4'
     },
     row:{
-        flex:1, 
+        flex:1,
         flexDirection: 'row',
         borderColor: '#000000',
         borderRadius:4,
@@ -115,14 +131,14 @@ const styles = StyleSheet.create({
     info:{
         textAlign: 'center',
         flex:1,
-        fontSize: 10, 
+        fontSize: 10,
         marginTop: 10
     },
     scheduleDisplay:{
         borderColor: "red",
         borderRadius:4,
-        borderWidth: 1, 
-        height: 500, 
+        borderWidth: 1,
+        height: 500,
         marginTop: 30
     }
-    });
+});
